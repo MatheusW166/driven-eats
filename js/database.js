8fa679b619e05b3687ba71e69bc1836902b6f8e8
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-03 01:40:08 -0400

    Initial commit

---
+++ js/database.js
@@ -0,0 +1,17 @@
+const [MAIN, DRINK, DESSERT] = ["MAIN", "DRINK", "DESSERT"];
+const db = [
+  {
+    foodId: 1,
+    category: MAIN,
+    name: "",
+    description: "",
+    img: "",
+    price: 0,
+  },
+];
+
+const order = {
+  MAIN: {},
+  DRINK: {},
+  DESSERT: {},
+};
